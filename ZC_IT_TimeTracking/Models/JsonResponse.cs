9be31ccf425a7ac374f7dc359efa498748848e93
dsavlanizc <dsavlani@zerochaos.com>
2015-11-04 16:41:31 +0530

    Unit test improved

---
+++ ZC_IT_TimeTracking/Models/JsonResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZC_IT_TimeTracking.Models
+{
+    public class JsonResponse
+    {
+        public string message { get; set; }
+        public bool success { get; set; }
+    }
+}
