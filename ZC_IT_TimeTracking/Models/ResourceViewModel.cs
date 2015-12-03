e6ba25f92f7f42e21ad2de9109be5972d8285b58
tpanchalzc <tpanchal@zerochaos.com>
2015-12-03 15:55:15 +0530

    LOGIN  ViewModel Added

---
+++ ZC_IT_TimeTracking/Models/ResourceViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZC_IT_TimeTracking.Models
+{
+    public class RegisterUserViewModel
+    {   
+        [Required]
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LName { get; set; }
+       
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public int Email { get; set; }
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+        
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+     
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }  
+    }
+   
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
