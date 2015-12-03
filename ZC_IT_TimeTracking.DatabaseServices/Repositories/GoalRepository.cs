56036207b0b5c97dc0bd761726e8a68789f31ffa
mgoswamizc <mgoswami@zerochaos.com>
2015-12-03 19:05:56 +0530

    Add Repository

---
+++ ZC_IT_TimeTracking.DatabaseServices/Repositories/GoalRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZC_IT_TimeTracking.Models;
+
+namespace ZC_IT_TimeTracking.DatabaseServices.Repositories
+{
+    public class GoalRepository
+    {
+        const string _CheckQuaterforResouces = "CheckQuater";
+        const string _AddGoalToGoalMaster = "InsertGoalMaster";
+        const string _AddQuarterForGoal = "InsertGoalQuarter";
+        const string _AddGoalRules = "InsertGoalRules";
+        const string _UpdateGoal = "UpdateGoalMaster";
+        const string _UpdateGoalRule = "UpdateGoalRules";
+        const string _GetAllGoalsOfResource = "GetAllGoalsOfResource";
+        const string _GetAllResourceForGoal = "GetAllResourceForGoal";
+        const string _GetGoalDetails = "GetGoalDetails";
+        const string _GetGoalRuleDetails = "GetGoalRuleDetails";
+        const string _GetQuarterDetails = "GetQuarterDetails";
+        const string _GetResouceDetails = "GetResouceDetails";
+        const string _GetResourceByTeam = "GetResourceByTeam";
+        const string _GetResourceGoalDetails = "GetResourceGoalDetails";
+        const string _getYearForQuarter = "getYearForQuarter";
+        const string _Delete_AllRulesOfGoal = "Delete_AllRulesOfGoal";
+        const string _DeleteGoalMaster = "DeleteGoalMaster";
+        const string _DeleteResourceGoal = "DeleteResourceGoal";
+        const string _DeleteGoalRule = "DeleteGoalRule";
+        const string _GetSpecificRecordsOfGoal = "GetSpecificRecordsOfGoal";
+        const string _GetQuarterFromYear = "GetQuarterFromYear";
+        const string _SearchGoalByTitle = "SearchGoalByTitle";
+    }
+
+    //public override Goal GoalDetail()
+    //{
+        
+
+    //}
+}
