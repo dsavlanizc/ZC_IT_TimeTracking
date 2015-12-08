f35566844a1e7b6aed29f39501c74395385cb616
dsavlanizc <dsavlani@zerochaos.com>
2015-12-08 15:48:44 +0530

    test cases corrected

--- ZC_IT_TimeTracking.Test/Controllers/HomeTest.cs
+++ ZC_IT_TimeTracking.Test/Controllers/HomeTest.cs
@@ -5,97 +5,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
-using ZC_IT_TimeTracking.Controllers;
-using ZC_IT_TimeTracking.Models;
+using ZC_IT_TimeTracking.BusinessEntities;
+using ZC_IT_TimeTracking.Services.Goals;
 
 namespace ZC_IT_TimeTracking.Test.Controllers
 {
     [TestClass]
     public class HomeTest
     {
-    //    [TestMethod]
-    //    public void GetGoalByIDTest()
-    //    {
-    //        HomeController home = new HomeController();
-    //        Object obj = home.GetGoalById(1).Data;
-    //        string success = obj.GetType().GetProperty("success").GetValue(obj, null).ToString();
-    //        Assert.AreEqual("True", success);
-    //    }
+        GoalServices _goalServices = new GoalServices();
+        DatabaseEntities dbCtx = new DatabaseEntities();
+        [TestMethod]
+        public void GetGoalByIDTest()
+        {
+            var goal = dbCtx.Goal_Master.OrderBy(o => o.Goal_MasterID).FirstOrDefault();
+            var goalDetails = _goalServices.GetGoaldetail(goal.Goal_MasterID);
+            Assert.AreEqual(goal.GoalTitle, goalDetails.GoalTitle);
+        }
 
-    //    [TestMethod]
-    //    public void CreateGoalTest()
-    //    {
-    //        HomeController home = new HomeController();
-    //        Goal goal = new Goal();
-    //        goal.Title = "Testing goal";
-    //        goal.Description = "goal testing description";
-    //        goal.UnitOfMeasurement = "Days";
-    //        goal.MeasurementValue = 12;
-    //        goal.Quarter = 4;
-    //        goal.Year = 2015;
-    //        GoalRule rule = new GoalRule();
-    //        rule.RangeFrom = 10;
-    //        rule.RangeTo = 50;
-    //        rule.Rating = 60;
-    //        goal.GoalRules = new List<GoalRule>();
-    //        goal.GoalRules.Add(rule);
-    //        JsonResponse asd = home.CreateGoal(goal).Data as JsonResponse;
-    //        Assert.AreEqual(true, asd.success);
-    //    }
+        [TestMethod]
+        public void CreateGoalTest()
+        {
+            Goal goal = new Goal();
+            goal.Title = "Testing goal";
+            goal.Description = "goal testing description";
+            goal.UnitOfMeasurement = "Days";
+            goal.MeasurementValue = 12;
+            goal.Quarter = 4;
+            goal.Year = 2015;
+            GoalRule rule = new GoalRule();
+            rule.RangeFrom = 10;
+            rule.RangeTo = 50;
+            rule.Rating = 60;
+            goal.GoalRules = new List<GoalRule>();
+            goal.GoalRules.Add(rule);
+            bool isSuccess = _goalServices.CreateGoal(goal);
+            Assert.AreEqual(true, isSuccess);
+        }
 
-    //    [TestMethod]
-    //    public void UpdateGoalTest()
-    //    {
-    //        HomeController home = new HomeController();
-    //        Goal goal = new Goal();
-    //        goal.ID = 2;
-    //        goal.Title = "Testing Update goal";
-    //        goal.Description = "Add goal description here";
-    //        goal.UnitOfMeasurement = "hours";
-    //        goal.MeasurementValue = 40;
-    //        goal.Quarter = 4;
-    //        goal.Year = 2015;
-    //        GoalRule rule = new GoalRule();
-    //        rule.RangeFrom = 70;
-    //        rule.RangeTo = 90;
-    //        rule.Rating = 80;
-    //        goal.GoalRules = new List<GoalRule>();
-    //        goal.GoalRules.Add(rule);
-    //        JsonResponse obj = home.UpdateGoal(goal).Data as JsonResponse;
-    //        Assert.AreEqual(true, obj.success);
-    //    }
+        [TestMethod]
+        public void UpdateGoalTest()
+        {
+            Goal goal = new Goal();
+            goal.ID = dbCtx.Goal_Master.Select(s=>s.Goal_MasterID).FirstOrDefault();
+            goal.Title = "Testing Update goal";
+            goal.Description = "Add goal description here";
+            goal.UnitOfMeasurement = "hours";
+            goal.MeasurementValue = 40;
+            goal.Quarter = 4;
+            goal.Year = 2015;
+            GoalRule rule = new GoalRule();
+            rule.RangeFrom = 70;
+            rule.RangeTo = 90;
+            rule.Rating = 80;
+            goal.GoalRules = new List<GoalRule>();
+            goal.GoalRules.Add(rule);
+            bool isSuccess = _goalServices.UpdateGoal(goal);
+            Assert.AreEqual(true, isSuccess);
+        }
 
-    //    [TestMethod]
-    //    public void DeleteGoalMaster()
-    //    {
-    //        int[] g = { 33 };
-    //        HomeController home = new HomeController();
-    //        JsonResponse obj = home.DeleteGoal(g).Data as JsonResponse;
-    //        Assert.AreEqual(true, obj.success);
-    //    }
-    //    //For Index
-    //    [TestMethod]
-    //    public void IndexTest()
-    //    {
-    //        HomeController idx = new HomeController();
-    //        ViewResult result = idx.Index() as ViewResult;
+        [TestMethod]
+        public void DeleteGoalMaster()
+        {
+            int[] goalId = { dbCtx.Goal_Master.Select(s => s.Goal_MasterID).FirstOrDefault() };
+            JsonResponse obj = _goalServices.DeleteGoal(goalId);
+            Assert.AreEqual(true, obj.success);
+        }
 
-    //        List<Goal_Master> list = new DatabaseEntities().Goal_Master.AsNoTracking().ToList();
-    //        List<Goal_Master> resultList = result.Model as List<Goal_Master>;
-    //        Assert.AreEqual(list.Count, resultList.Count);
-    //    }
-
-    //    [TestMethod]
-    //    public void AddQuarterTest()
-    //    {
-    //        HomeController home = new HomeController();
-    //        GoalQuarters Quarter = new GoalQuarters();
-    //        Quarter.GoalQuarter = 3;
-    //        Quarter.QuarterYear = 2010;
-    //        Quarter.GoalCreateFrom = DateTime.Today.AddYears(-5);
-    //        Quarter.GoalCreateTo = DateTime.Today.AddYears(-5);
-    //        JsonResponse obj = home.AddQuarter(Quarter).Data as JsonResponse;
-    //        Assert.AreEqual(true, obj.success);
-    //    }
+        [TestMethod]
+        public void AddQuarterTest()
+        {
+            GoalQuarters Quarter = new GoalQuarters();
+            Quarter.GoalQuarter = 3;
+            Quarter.QuarterYear = 2010;
+            Quarter.GoalCreateFrom = DateTime.Today.AddYears(-5);
+            Quarter.GoalCreateTo = DateTime.Today.AddYears(-5);
+            JsonResponse obj = _goalServices.CreateQuarter(Quarter);
+            Assert.AreEqual(true, obj.success);
+        }
     }
 }
